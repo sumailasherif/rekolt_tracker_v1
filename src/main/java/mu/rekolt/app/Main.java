5cdbbc36409235d21f3c8184b7d330ebcbf2bcd2
Sherif <sumailasherif773@gmail.com>
2026-08-17 10:39:38 +0400

    created a structure for the worked example before proceeding to create dependables for other condition

--- src/main/java/mu/rekolt/app/Main.java
+++ src/main/java/mu/rekolt/app/Main.java
@@ -1,7 +1,29 @@
 package mu.rekolt.app;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Rekolt Tracker starting...");
+        // 1. We declare inputs
+        double mass = 236.0;
+        double basePrice = 90.0;
+        // This helps us to calculate the Base Value. we use double to cater for decimals
+        double baseValue = mass * basePrice;
+
+        // We declare Grade Multiplier (Grade A = 1.15)
+        double gradeMultiplier = 1.15;
+        double gradeValue = baseValue * gradeMultiplier;
+
+        //We apply Category Multiplier (Cereal = 1.00)
+        double categoryMultiplier = 1.00;
+        double categoryValue = gradeValue * categoryMultiplier;
+
+        //we calculate the commission (5% or 0.05)
+        double commission = categoryValue * 0.05;
+
+        //transport levy is MUR 2 per kg
+        double transportLevy = mass * 2.0;
+
+        //Net Payable is calculated difference between category value and commission and transport
+        double netPayable = categoryValue - commission - transportLevy;
+        System.out.println("Net Payable: " + netPayable);
     }
 
 
